8bc9040a8e6293ac6a8f444aaa57aa561ef178a2
Sin0001 <bboyliljie@gmail.com>
2021-09-15 17:02:40 +0800

    Add generate_password.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const password = generatePassword(req.body)
+  res.render('index', {password: password})
 })
 
 app.listen(port, () => {
